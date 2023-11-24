c36678d177bb5d2343cfe3c6edc69074e0cdd50a
Jason Park <jason808@naver.com>
2023-11-24 10:52:21 +0900

    Fixed 불필요한 active logic

--- ai_code_helper/src/components/MenuDiv.js
+++ ai_code_helper/src/components/MenuDiv.js
@@ -1,30 +1,18 @@
 import "../styles/MenuDiv.css";
 
 function MenuDiv({ curMenu, operation, setCurMenu, children }) {
- 
   // active 클래스 추가 로직
   const isActive = curMenu === operation;
   return (
-    <div
-      className={`MenuDiv ${isActive ? "active" : ""}`}
-    >
-      
+    <div className={`MenuDiv ${isActive ? "active" : ""}`}>
       <label onClick={() => setCurMenu(operation)}>{operation}</label>
       {isActive ? (
         <div className="content-container">
-          <div className="content">
-            {isActive ? (
-              <div class="content">
-                {children}
-              </div>
-            ) : null}
-          </div>
+            <div className="content">{children}</div>
         </div>
       ) : null}
 
-      logic + html 
-      <div>
-      </div>
+      <div></div>
     </div>
   );
 }
