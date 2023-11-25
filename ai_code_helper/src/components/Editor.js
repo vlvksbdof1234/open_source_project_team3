38fb319faab98644fb71fd2dbec95e6d0b1ad83d
vlvksbdof1234 <vlvksbdof12@daum.net>
2023-11-25 20:49:02 +0900

    codespace 틀 잡기

---
+++ ai_code_helper/src/components/Editor.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import AceEditor from "react-ace";
+// import "ace-builds/src-noconflict/theme-chaos";
+// import "ace-builds/src-noconflict/theme-github";
+// import "ace-builds/src-noconflict/ext-language_tools.js";
+
+// const ace = require("ace-builds/src-noconflict/ace");
+// ace.config.set(
+//   "basePath",
+//   "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/"
+// );
+// ace.config.setModuleUrl(
+//   "ace/mode/javascript_worker",
+//   "https://cdn.jsdelivr.net/npm/ace-builds@1.4.3/src-noconflict/worker-javascript.js"
+// );
+
+export default function Editor(data) {
+  return (
+    <div></div>
+  );
+}
