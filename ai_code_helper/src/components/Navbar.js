8fc0879aabc244b535a3010ce7239a82798b0c67
Jason Park <jason808@naver.com>
2023-11-26 00:36:38 +0900

    Navbar 추가

---
+++ ai_code_helper/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Navbar.css"
+
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar">
+        <i>AI code helper</i>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
