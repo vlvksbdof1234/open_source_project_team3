1cd9cbe7f861b19dd94d5112d73bb8e5706a1c63
Jason Park <jason808@naver.com>
2023-11-27 15:30:39 +0900

    flow chart update

--- ai_code_helper/src/components/FlowChartDiv.js
+++ ai_code_helper/src/components/FlowChartDiv.js
@@ -1,28 +1,75 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import mermaid from "mermaid";
-import "../styles/FlowChartDiv.css"
+import "../styles/FlowChartDiv.css";
 
 export const FlowChartDiv = () => {
-  // Store Mermaid code
-  const mermaidCode = `
+  const initialCode = `
     graph TD;
     A-->B;
     A-->C;
     B-->D;
     C-->D;
   `;
-  
-  useEffect(() => {
-    // Mermaid 초기화
-    mermaid.initialize({ startOnLoad: true });
+  const [mermaidCode, setMermaidCode] = useState(initialCode);
+  const [currentCode, setCurrentCode] = useState(initialCode);
+  const [isError, setIsError] = useState(false);
 
-    // Mermaid 코드 렌더링
-    mermaid.contentLoaded();
+  const renderMermaid = () => {
+    mermaid.initialize({
+      startOnLoad: true,
+      errorCallback: (err) => {
+        console.error(err);
+        setIsError(true);
+      },
+    });
+
+    // Clear the previous diagram
+    const container = document.querySelector(".mermaid");
+    if (container) {
+      container.innerHTML = "";
+    }
+
+    mermaid.render("mermaidGraph", mermaidCode, (svgCode) => {
+      if (container) {
+        container.innerHTML = svgCode;
+      }
+      setIsError(false);
+    });
+  };
+
+  useEffect(() => {
+    renderMermaid();
   }, []);
 
+  const handleCodeChange = (event) => {
+    setCurrentCode(event.target.value);
+  };
+
+  const executeMermaid = () => {
+    setMermaidCode(currentCode);
+    renderMermaid();
+  };
+
+  const resetCode = () => {
+    setCurrentCode(initialCode);
+    setMermaidCode(initialCode);
+    renderMermaid();
+  };
+
   return (
-    <div>
-      <div className="mermaid">{mermaidCode}</div>
+    <div className="mermaid-layout">
+      <div className="mermaid-editor">
+        <textarea value={currentCode} onChange={handleCodeChange} />
+        <button onClick={executeMermaid}>실행</button>
+        <button onClick={resetCode}>원래대로</button>
+      </div>
+      <div className="mermaid-container">
+        {isError ? (
+          <img src="/path/to/error-image.png" alt="Error" />
+        ) : (
+          <div className="mermaid" id="mermaidGraph"></div>
+        )}
+      </div>
     </div>
   );
 };
