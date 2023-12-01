8318de33dceeb52c525f490f8e727175ed67e91a
Jason Park <jason808@naver.com>
2023-12-02 00:47:30 +0900

    Back to working version

--- ai_code_helper/src/components/FlowChartDiv.js
+++ ai_code_helper/src/components/FlowChartDiv.js
@@ -3,73 +3,43 @@ import mermaid from "mermaid";
 import "../styles/FlowChartDiv.css";
 
 export const FlowChartDiv = () => {
-  const initialCode = `
+  // Store Mermaid code
+  const mermaidCode = `
     graph TD;
     A-->B;
     A-->C;
     B-->D;
     C-->D;
   `;
-  const [mermaidCode, setMermaidCode] = useState(initialCode);
-  const [currentCode, setCurrentCode] = useState(initialCode);
-  const [isError, setIsError] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const renderMermaid = () => {
-    mermaid.initialize({
-      startOnLoad: true,
-      errorCallback: (err) => {
-        console.error(err);
-        setIsError(true);
-      },
-    });
-
-    // Clear the previous diagram
-    const container = document.querySelector(".mermaid");
-    if (container) {
-      container.innerHTML = "";
-    }
-
-    mermaid.render("mermaidGraph", mermaidCode, (svgCode) => {
-      if (container) {
-        container.innerHTML = svgCode;
-      }
-      setIsError(false);
-    });
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
   useEffect(() => {
-    renderMermaid();
+    // Mermaid 초기화
+    mermaid.initialize({ startOnLoad: true, maxWidth: 800 });
+
+    // Mermaid 코드 렌더링
+    mermaid.contentLoaded();
+    const mermaidSvg = document.querySelector(".mermaid svg");
+    if (mermaidSvg) {
+      mermaidSvg.style.maxWidth = "250px";
+      mermaidSvg.style.width = "100%";
+      mermaidSvg.style.height = "auto";
+    }
   }, []);
 
-  const handleCodeChange = (event) => {
-    setCurrentCode(event.target.value);
-  };
-
-  const executeMermaid = () => {
-    setMermaidCode(currentCode);
-    renderMermaid();
-  };
-
-  const resetCode = () => {
-    setCurrentCode(initialCode);
-    setMermaidCode(initialCode);
-    renderMermaid();
-  };
-
   return (
-    <div className="mermaid-layout">
-      <div className="mermaid-editor">
-        <textarea value={currentCode} onChange={handleCodeChange} />
-        <button onClick={executeMermaid}>실행</button>
-        <button onClick={resetCode}>원래대로</button>
+    <div className="mermaid-container">
+      <div
+        className={`mermaid ${isModalOpen ? "modal" : ""}`}
+        onClick={toggleModal}
+      >
+        {mermaidCode}
       </div>
-      <div className="mermaid-container">
-        {isError ? (
-          <img src="/path/to/error-image.png" alt="Error" />
-        ) : (
-          <div className="mermaid" id="mermaidGraph"></div>
-        )}
-      </div>
+      {isModalOpen && <div className="modal-backdrop"></div>}
     </div>
   );
 };
