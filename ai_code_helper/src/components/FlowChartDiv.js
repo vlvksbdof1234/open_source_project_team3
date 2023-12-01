c8bd222b3c6d6eb13873cf679c5a65392045a621
Jason Park <jason808@naver.com>
2023-12-02 01:56:15 +0900

    다이어그램 변경 구현 완료

--- ai_code_helper/src/components/FlowChartDiv.js
+++ ai_code_helper/src/components/FlowChartDiv.js
@@ -3,42 +3,64 @@ import mermaid from "mermaid";
 import "../styles/FlowChartDiv.css";
 
 export const FlowChartDiv = () => {
-  // Store Mermaid code
-  const mermaidCode = `
+  const initialMermaidCode = `
     graph TD;
     A-->B;
     A-->C;
     B-->D;
     C-->D;
   `;
+  const [mermaidCode, setMermaidCode] = useState(initialMermaidCode);
+  const [renderedMermaidCode, setRenderedMermaidCode] = useState(initialMermaidCode);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
-  useEffect(() => {
-    // Mermaid 초기화
-    mermaid.initialize({ startOnLoad: true, maxWidth: 800 });
+  // Mermaid 다이어그램 렌더링 함수
+  const renderMermaid = () => {
+    mermaid.initialize({ startOnLoad: true });
 
-    // Mermaid 코드 렌더링
-    mermaid.contentLoaded();
-    const mermaidSvg = document.querySelector(".mermaid svg");
-    if (mermaidSvg) {
-      mermaidSvg.style.maxWidth = "250px";
-      mermaidSvg.style.width = "100%";
-      mermaidSvg.style.height = "auto";
+    const mermaidElement = document.getElementById('mermaid-diagram');
+    if (mermaidElement.getAttribute('data-processed')) {
+      mermaidElement.removeAttribute('data-processed');
     }
-  }, []);
+
+    mermaid.contentLoaded();
+  };
+
+  const applyChanges = () => {
+    setRenderedMermaidCode(mermaidCode);
+  };
+
+  // 초기화 버튼 핸들러
+  const resetCode = () => {
+    setMermaidCode(initialMermaidCode);
+    setRenderedMermaidCode(initialMermaidCode);
+  };
+
+  useEffect(() => {
+    renderMermaid();
+  }, [renderedMermaidCode]);
 
   return (
     <div className="mermaid-container">
       <div
         className={`mermaid ${isModalOpen ? "modal" : ""}`}
         onClick={toggleModal}
-      >
-        {mermaidCode}
+        id="mermaid-diagram"
+      >{renderedMermaidCode}</div>
+      <div className="mermaid-editor">
+        <textarea
+          value={mermaidCode}
+          onChange={(e) => setMermaidCode(e.target.value)}
+        />
+        <button onClick={applyChanges}>적용</button>
+        <button onClick={resetCode}>초기화</button>
       </div>
+      
+      
       {isModalOpen && <div className="modal-backdrop"></div>}
     </div>
   );
